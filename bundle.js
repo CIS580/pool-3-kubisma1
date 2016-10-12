1f6d3c509e8021946a1402875a095fa5f5d13281
Martin Kubis <mkubis@ksu.edu>
2016-10-12 15:18:36 -0500

    In-class work

--- bundle.js
+++ bundle.js
@@ -3,6 +3,7 @@
 
 /* Classes */
 const Game = require('./game');
+const Vector = require('./vector');
 
 /* Global variables */
 var canvas = document.getElementById('screen');
@@ -247,6 +248,38 @@ function update(elapsedTime) {
   });
 
   // TODO: Process ball collisions
+  collisions.forEach(function(pair) {
+    // find the normal of collision
+    var collisionNormal = {
+      x: pair.a.position.x - pair.b.position.x,
+      y: pair.a.position.y - pair.b.position.y
+    }
+    // calculate the overlap between balls, each ball has a radius equal to 15
+    var overlap = 32 - Vector.magnitude(collisionNormal);
+    collisionNormal = Vector.normalize(collisionNormal);
+    pair.a.position.x += collisionNormal.x * overlap / 2;
+    pair.a.position.y += collisionNormal.y * overlap / 2;
+    pair.b.position.x -= collisionNormal.x * overlap / 2;
+    pair.b.position.y -= collisionNormal.y * overlap / 2;
+    // tg theta = y / x
+    // theta = acrtan (y/x)
+    var angle = Math.atan2(collisionNormal.y, collisionNormal.x);
+    // rotate the problem space so that the normalize
+    // of collision lies along the x-axis
+    var a = Vector.rotate(pair.a.velocity, angle);
+    var b = Vector.rotate(pair.b.velocity, angle);
+    // solve the collision along the x-axis
+    var s = a.x;
+    a.x = b.x;
+    b.x = s;
+    // Rotate the problem space back to world space
+    a = Vector.rotate(a, -angle);
+    b = Vector.rotate(b, -angle);
+    pair.a.velocity.x = a.x;
+    pair.a.velocity.y = a.y;
+    pair.b.velocity.x = b.x;
+    pair.b.velocity.y = b.y;
+  });
 }
 
 /**
@@ -305,7 +338,7 @@ function render(elapsedTime, ctx) {
   ctx.beginPath();
 }
 
-},{"./game":2}],2:[function(require,module,exports){
+},{"./game":2,"./vector":3}],2:[function(require,module,exports){
 "use strict";
 
 /**
@@ -361,6 +394,40 @@ Game.prototype.loop = function(newTime) {
 
   // Flip the back buffer
   this.frontCtx.drawImage(this.backBuffer, 0, 0);
+}
+
+},{}],3:[function(require,module,exports){
+module.exports = exports = {
+  rotate: rotate,
+  dotProduct: dotProduct,
+  magnitude: magnitude,
+  normalize: normalize
+}
+
+/**
+ * Stands for matrix multiplication  {x,y} * {{cos phi, -sin phi}, {sin phi, cos phi}}
+ */
+function rotate(a, angle) {
+  return {
+    x: a.x * Math.cos(angle) - a.y * Math.sin(angle),
+    y: a.x * Math.sin(angle) + a.y * Math.cos(angle)
+  }
+}
+
+function dotProduct(a, b) {
+  return a.x * b.x + a.y * b.y;
+}
+
+function magnitude(a) {
+  return Math.sqrt(a.x * a.x + a.y * a.y);
+}
+
+function normalize(a) {
+  var mag = magnitude(a);
+  return {
+    x: a.x / mag,
+    y: a.y / mag
+  };
 }
 
 },{}]},{},[1]);
--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 
 /* Classes */
 const Game = require('./game');
+const Vector = require('./vector');
 
 /* Global variables */
 var canvas = document.getElementById('screen');
@@ -246,6 +247,38 @@ function update(elapsedTime) {
   });
 
   // TODO: Process ball collisions
+  collisions.forEach(function(pair) {
+    // find the normal of collision
+    var collisionNormal = {
+      x: pair.a.position.x - pair.b.position.x,
+      y: pair.a.position.y - pair.b.position.y
+    }
+    // calculate the overlap between balls, each ball has a radius equal to 15
+    var overlap = 32 - Vector.magnitude(collisionNormal);
+    collisionNormal = Vector.normalize(collisionNormal);
+    pair.a.position.x += collisionNormal.x * overlap / 2;
+    pair.a.position.y += collisionNormal.y * overlap / 2;
+    pair.b.position.x -= collisionNormal.x * overlap / 2;
+    pair.b.position.y -= collisionNormal.y * overlap / 2;
+    // tg theta = y / x
+    // theta = acrtan (y/x)
+    var angle = Math.atan2(collisionNormal.y, collisionNormal.x);
+    // rotate the problem space so that the normalize
+    // of collision lies along the x-axis
+    var a = Vector.rotate(pair.a.velocity, angle);
+    var b = Vector.rotate(pair.b.velocity, angle);
+    // solve the collision along the x-axis
+    var s = a.x;
+    a.x = b.x;
+    b.x = s;
+    // Rotate the problem space back to world space
+    a = Vector.rotate(a, -angle);
+    b = Vector.rotate(b, -angle);
+    pair.a.velocity.x = a.x;
+    pair.a.velocity.y = a.y;
+    pair.b.velocity.x = b.x;
+    pair.b.velocity.y = b.y;
+  });
 }
 
 /**
